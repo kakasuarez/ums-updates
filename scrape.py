11fca6c755078e62ddb784f4417891e706350aac
Vatsal Veerwal <vatsalveerwal@gmail.com>
2024-07-20 18:49:02 +0530

    Made working with notices without a URL.

--- scrape.py
+++ scrape.py
@@ -1,29 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
+import re
+
 
 class Notice:
-	def __init__(self, row):
-		self.date_uploaded = datetime.strptime(row.find_previous_sibling("td").font.contents[0].strip(), "%d-%m-%Y")
-		self.title = row.contents[0].font.contents[0]
-		self.url = row.contents[0].get("href")
-		self.publisher = row.contents[2].b.contents[14:]
+    def __init__(self, row):
+        self.text = row.text
+        self.url = row.contents[0].get("href")
+        part = self.text.partition("Published By: ")
+        self.title = part[0]
+        self.publisher = part[2]
+
+    def escaped_title(self):
+        markdown_chars = r"\\`*_{}[]()#+-.!|"
+        escaped_text = re.sub(
+            r"([{}])".format(re.escape(markdown_chars)), r"\\\1", self.title
+        )
+        return escaped_text
 
 
 class Scraper:
-	def __init__(self):
-		url = "https://www.imsnsit.org/imsnsit/notifications.php"
-		document = requests.get(url)
-		self.parse = BeautifulSoup(document.text, "html.parser")
-		self.last_seen_notice = "ALL THE COURSE INSTRUCTORS OF THE UNIVERSITY MAY KINDLY COMPLETE THE ATTENDANCE ENTRY, FOR THE PERIOD 02.01.2024 TO 09.01.2024, ON CUMS LATEST BY 17.01.2024"
-		
-	def get_new_notices(self):
-		new_notices = []
-		for row in self.parse.find_all("td", class_="list-data-focus"):
-			current_notice = Notice(row)
-			current_notice_title = current_notice.title
-			if current_notice_title == self.last_seen_notice:
-				if new_notices != []:
-					self.last_seen_notice = new_notices[0].title
-				return new_notices
-			new_notices.append(current_notice)
+    def __init__(self, last_seen_notice):
+        url = "https://www.imsnsit.org/imsnsit/notifications.php"
+        document = requests.get(url)
+        self.parse = BeautifulSoup(document.text, "html.parser")
+        self.last_seen_notice = last_seen_notice
+
+    def get_new_notices(self):
+        new_notices = []
+        rows = self.parse.find_all("td", class_="list-data-focus")
+        for row in rows:
+            current_notice = Notice(row)
+            current_notice_title = current_notice.title
+            if current_notice_title == self.last_seen_notice:
+                if new_notices:
+                    self.last_seen_notice = new_notices[0]
+                return new_notices
+            new_notices.append(current_notice)
