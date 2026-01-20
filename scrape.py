8a7aeb64e41425225bb9b311d2c036389f4e1715
Vatsal Veerwal <vatsalveerwal@gmail.com>
2026-01-20 17:52:47 +0530

    Get new notices from same `Scraper` instance

--- scrape.py
+++ scrape.py
@@ -20,20 +20,32 @@ class Notice:
 
 
 class Scraper:
-    def __init__(self, last_seen_notice):
-        url = "https://www.imsnsit.org/imsnsit/notifications.php"
-        document = requests.get(url)
-        self.parse = BeautifulSoup(document.text, "html.parser")
-        self.last_seen_notice = last_seen_notice
+    def __init__(self):
+        self.url = "https://www.imsnsit.org/imsnsit/notifications.php"
+        self.refresh_documents()
+        self.last_seen_notice = self.get_latest_notice()
+
+    def refresh_documents(self):
+        self.document = requests.get(self.url)
+        self.parse = BeautifulSoup(self.document.text, "html.parser")
+
+    def get_all_notices(self):
+        self.refresh_documents()
+        return self.parse.find_all("td", class_="list-data-focus")
 
     def get_new_notices(self):
+        self.refresh_documents()
         new_notices = []
-        rows = self.parse.find_all("td", class_="list-data-focus")
+        rows = self.get_all_notices()
         for row in rows:
             current_notice = Notice(row)
             current_notice_title = current_notice.title
-            if current_notice_title == self.last_seen_notice:
+            if current_notice_title == self.last_seen_notice.title:
                 if new_notices:
-                    self.last_seen_notice = new_notices[0].title
+                    self.last_seen_notice = new_notices[0]
                 return new_notices
             new_notices.append(current_notice)
+        return []
+
+    def get_latest_notice(self):
+        return Notice(self.get_all_notices()[0])
